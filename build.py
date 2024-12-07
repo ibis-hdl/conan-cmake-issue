a0d186daffb723766c741db828513262d05dd274
ibis-hdl <83083329+ibis-hdl@users.noreply.github.com>
2024-12-07 16:46:38 +0000

    call of conan install explicit

--- build.py
+++ build.py
@@ -9,8 +9,69 @@ except:
     pass
 
 run("conan version")
+
+"""
+$ conan profile show -pr=clang
+Host profile:
+[settings]
+arch=x86_64
+build_type=Release
+compiler=clang
+compiler.cppstd=gnu17
+compiler.libcxx=libstdc++11
+compiler.version=18
+os=Linux
+[conf]
+tools.build:compiler_executables={'c': '/usr/bin/clang', 'cpp': '/usr/bin/clang++'}
+[buildenv]
+CC=clang
+CXX=clang++
+
+Build profile:
+[settings]
+arch=x86_64
+build_type=Release
+compiler=gcc
+compiler.cppstd=gnu17
+compiler.libcxx=libstdc++11
+compiler.version=13
+os=Linux
+"""
+
+run("conan install . --settings build_type=Release --conf tools.cmake.cmaketoolchain:generator='Ninja Multi-Config' --build=missing --profile:all=clang")
 run('cmake --preset clang')
 run('cmake --build --preset clang-release')
+
+"""
+$ conan profile show -pr=clang-libc++
+Host profile:
+[settings]
+arch=x86_64
+build_type=Release
+compiler=clang
+compiler.cppstd=gnu17
+compiler.libcxx=libc++
+compiler.version=18
+os=Linux
+[conf]
+tools.build:compiler_executables={'c': '/usr/bin/clang', 'cpp': '/usr/bin/clang++'}
+[buildenv]
+CC=clang
+CXX=clang++
+
+Build profile:
+[settings]
+arch=x86_64
+build_type=Release
+compiler=gcc
+compiler.cppstd=gnu17
+compiler.libcxx=libstdc++11
+compiler.version=13
+os=Linux
+"""
+
+run("conan install . --settings build_type=Release --conf tools.cmake.cmaketoolchain:generator='Ninja Multi-Config' --build=missing --profile:all=clang-libc++")
 run('cmake --preset clang-libc++')
 run('cmake --build --preset clang-libc++release')
+
 run("cmake --list-presets")
